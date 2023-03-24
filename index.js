7eaf10a5c43bf57ebea5f2f17e4d1a4116a6c142
Ainsley Alceme <youngchill101@gmail.com>
2023-03-24 14:40:55 -0400

    submits

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+/*function (){
+    var customerName = 'bob'
+    return customerName
+}*/
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = (customerName.toUpperCase())
+}
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+
+function overwriteBestCustomer(){
+   return  bestCustomer = 'maybe bob'
+}
+
+
+
+const leastFavoriteCustomer = "kam"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'dame'
+}
